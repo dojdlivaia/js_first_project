ffcc5013b366ba5561d6a7c41453b5a86c22e248
Irina Tiuzhina <dojdlivaia@bk.ru>
2020-06-01 00:53:28 +0400

    change script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 let number = 5;
 const leftBorderWidth = 1;
-const answer = prompt ("Вам есть 18?"," ");
-console.log(answer)
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?","0");
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    gendors : [],
+    privat : false
+};
+const a = prompt ("Один из последних просмотренных фильмов?"," "),
+      b = prompt('На сколько оцените его?',""),
+      c = prompt ("Один из последних просмотренных фильмов?"," "),
+      d = prompt('На сколько оцените его?',"");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
